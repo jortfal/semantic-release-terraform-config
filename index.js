09c4125e15d9d61f295e4f7a4c5f509e6caf0fe6
jortfal | José Manuel Ortega Falcón <jortfal@users.noreply.github.com>
2021-04-18 11:36:03 +0200

    release: v1.0.0 (#3)
    * docs: add license
    
    * docs: add readme
    
    * ci: add test workflow
    
    * style: prettier code formatter
    
    * ci(test): improve workflow
    
    * ci(release): add release workflow
    
    * release: v1.0.0 (#1)
    
    * chore: add gitpod config
    
    * feat: add package config
    
    * chore: add package lock
    
    * feat: add shareable configuration
    
    * docs: add license
    
    * docs: add readme
    
    * ci: add test workflow
    
    * style: prettier code formatter
    
    * ci(test): improve workflow
    
    * ci(release): add release workflow
    
    * ci(release): change name branch master by main in on trigger

--- index.js
+++ index.js
@@ -1,26 +1,49 @@
+/* 
+Copyright 2021 @jortfal | José Mnauel Ortega Falcón
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. 
+*/
+
 module.exports = {
-    plugins: [
-      '@semantic-release/commit-analyzer',
-      '@semantic-release/release-notes-generator',
-      '@semantic-release/github',
-      '@semantic-release/changelog',
-      ["@google/semantic-release-replace-plugin", {
-        "replacements": [{
-            "files": ["locals.tf"],
-            "from": "\"Terraform Module Version\" = \".*\"",
-            "to": "\"Terraform Module Version\" = \"${nextRelease.version}\"",
-            "results": [{
-                "file": "locals.tf",
-                "hasChanged": true,
-                "numMatches": 1,
-                "numReplacements": 1
-            }],
-            "countMatches": true
-        }]
-      }],
-      ["@semantic-release/git", {
-        "assets": ["locals.tf", "CHANGELOG.md"],
-        "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-      }]
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/github',
+    '@semantic-release/changelog',
+    [
+      '@google/semantic-release-replace-plugin',
+      {
+        replacements: [
+          {
+            files: ['locals.tf'],
+            from: `"Terraform Module Version" = ".*"`,
+            to: `"Terraform Module Version" = "\${nextRelease.version}"`,
+            results: [
+              {
+                file: 'locals.tf',
+                hasChanged: true,
+                numMatches: 1,
+                numReplacements: 1,
+              },
+            ],
+            countMatches: true,
+          },
+        ],
+      },
     ],
-  };
+    [
+      '@semantic-release/git',
+      {
+        assets: ['locals.tf', 'CHANGELOG.md'],
+        message: `chore(release): \${nextRelease.version} [skip ci]\n\n\${nextRelease.notes}`,
+      },
+    ],
+  ],
+};
